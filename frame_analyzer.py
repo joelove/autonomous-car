b23a54cbeb93112c8b7774932b0d21e30f956bcb
Joe Love <git@joelove.uk>
2019-04-03 18:09:12 +0100

    add gitignore

--- frame_analyzer.py
+++ frame_analyzer.py
@@ -2,7 +2,7 @@ import cv2
 
 def handle_frame(frame):
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(image, 127, 255, 0)
+    ret, thresh = cv2.threshold(image, 200, 255, 0)
 
     contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
