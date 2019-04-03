3f86ebe3278d8a7e99fe2dfbcb630d66da262cc0
Joe Love <git@joelove.uk>
2019-04-03 16:37:07 +0100

    add code to capture camera input and call handler

---
+++ camera.py
@@ -0,0 +1,20 @@
+import cv2
+import time
+
+def capture(screen, handle_frame):
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        video_capture.grab()
+        ret, frame = video_capture.retrieve()
+
+        if (ret == False):
+            break;
+
+        image = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+
+        handle_frame(image)
+
+        time.sleep(0)
+
+    video_capture.release()
