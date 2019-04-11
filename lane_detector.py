86919f89e94c8fc61da95753d5f18d4a83df04b4
Joe Love <git@joelove.uk>
2019-04-11 23:05:39 +0100

    add camera calibration script and unpick lane detector

--- lane_detector.py
+++ lane_detector.py
@@ -1,60 +1,28 @@
 import cv2
 import numpy as np
 
-
-# def undistort_image():
-#     obj_pts = np.zeros((6*9, 3), np.float32)
-#     obj_pts[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
-#
-#     objpoints = []
-#     imgpoints = []
-#
-#     images = glob.glob('camera_calibration/calibration_images/*.jpg')
-#
-#     for indx, fname in enumerate(images):
-#         img = cv2.imread(fname)
-#         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#         ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
-#
-#         if ret == True:
-#             objpoints.append(obj_pts)
-#             imgpoints.append(corners)
-#
-#     img_size = (img.shape[1], img.shape[0])
-#     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
-#     dst = cv2.undistort(img, mtx, dist, None, mtx)
-#
-#     dist_pickle = {}
-#     dist_pickle['mtx'] = mtx
-#     dist_pickle['dist'] = dist
-#
-#     pickle.dump( dist_pickle, open('camera_calibration/calibration_pickle.p', 'wb') )
+from funcy import compose
 
 
-# def undistort(img, calibration_directory='camera_calibration/calibration_pickle.p'):
-#     with open(calibration_directory, mode='rb') as f:
-#         file = pickle.load(f)
-#         mtx = file['mtx']
-#         dist = file['dist']
-#         dst = cv2.undistort(img, mtx, dist, None, mtx)
-#         # cv2.imwrite('camera_calibration/test_calibration.jpg', dst)
-#         return dst
+reverse_tuple = compose(reversed, tuple)
 
 
-def perspective_warp(image,
-        dst_size=(1280,720),
-        src=np.float32([(0.43, 0.65), (0.58, 0.65), (0.1, 1), (1, 1)]),
-        dst=np.float32([(0, 0), (1, 0), (0, 1), (1, 1)])):
-    image_size = np.float32([(image.shape[1], image.shape[0])])
-    src = src * image_size
-    dst = dst * np.float32(dst_size)
-    M = cv2.getPerspectiveTransform(src, dst)
-    warped = cv2.warpPerspective(image, M, dst_size)
-    return warped
+def perspective_warp(image):
+    image_dimensions = reverse_tuple(image.shape)
+
+    source_shape = [(0.43, 0.65), (0.58, 0.65), (0.1, 1), (1, 1)]
+    destination_shape = [(0, 0), (1, 0), (0, 1), (1, 1)]
+
+    source_pixels = np.float32(source_shape) * np.float32(image_dimensions)
+    destination_pixels = np.float32(destination_shape) * np.float32(image_dimensions)
+
+    transformation = cv2.getPerspectiveTransform(source_pixels, destination_pixels)
+
+    return cv2.warpPerspective(image, transformation, image_dimensions)
 
 
-def reduce_noise(image, ksize=3):
-    return cv2.GaussianBlur(image, (ksize, ksize), 0)
+def reduce_noise(image):
+    return cv2.GaussianBlur(image, (3, 3), 0)
 
 
 def rgb_to_grayscale(image):
@@ -64,10 +32,6 @@ def rgb_to_grayscale(image):
 def calculate_image_derivatives(image):
     scharr_x = cv2.Scharr(image, cv2.CV_16S, 1, 0)
     scharr_y = cv2.Scharr(image, cv2.CV_16S, 0, 1)
-
-    # # A kernel size of 5 will be more computationally expensive but potentially more useful
-    # sobel_x = cv2.Sobel(img, cv2.CV_16S, 1, 0, ksize=5)
-    # sobel_y = cv2.Sobel(img, cv2.CV_16S, 0, 1, ksize=5)
 
     x_derivative = cv2.convertScaleAbs(scharr_x)
     y_derivative = cv2.convertScaleAbs(scharr_y)
@@ -82,18 +46,8 @@ def detect_edges(image):
     return edges
 
 
-def convert_to_8_bit(image):
-    return cv2.convertScaleAbs(image)
-
-
 def detect_lanes(image):
-    denoised_image = reduce_noise(image)
-    greyscale_image = rgb_to_grayscale(denoised_image)
-    perspective_image = perspective_warp(greyscale_image)
-    edges = detect_edges(perspective_image)
+    apply_filters = compose(detect_edges, perspective_warp, rgb_to_grayscale, reduce_noise)
+    filtered_image = apply_filters(image)
 
-    histogram = cv2.calcHist([edges], [0], None, [256], [0, 256])
-
-    print(histogram)
-
-    # cv2.imshow('Preview', edges)
+    cv2.imshow('Preview', filtered_image)
