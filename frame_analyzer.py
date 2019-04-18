e2744c8dc8fbf807567b4217eb917f8289bf7de7
Joe Love <git@joelove.uk>
2019-04-18 15:52:59 +0100

    just save frame to test image quality

--- frame_analyzer.py
+++ frame_analyzer.py
@@ -4,5 +4,5 @@ import image_processor
 def handle_frame(frame):
     filtered_image = image_processor.apply_filters(frame)
 
-    cv2.imwrite('preview.jpg', filtered_image)
+    cv2.imwrite('preview.jpg', frame)
     # cv2.imshow('Preview', filtered_image)
