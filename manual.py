bbfec871d53ab4844212111c81eb7fb22c697323
Joe Love <git@joelove.uk>
2019-04-26 16:36:34 +0100

    update record path

--- manual.py
+++ manual.py
@@ -39,8 +39,9 @@ class Manual:
         print(throttle) # debug
 
         timestamp = time.time()
+        record_path = f'{config.DATA_PATH}/{timestamp}_record.json'
 
-        with open(f'{config.DATA_PATH}/{timestamp}_record.json', 'w') as record_file:
+        with open(record_path, 'w') as record_file:
             json.dump({ timestamp, throttle, angle, frame_array }, record_file)
 
 
