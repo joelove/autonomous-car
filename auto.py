ccf1d9edd7e804f2ee5192901613e797f9f2e578
Joe Love <git@joelove.uk>
2019-04-26 16:14:40 +0100

    add predicitons from model to auto class

--- auto.py
+++ auto.py
@@ -1,13 +1,32 @@
+import cv2
+import numpy as np
 import config
 
+from keras.models import model_from_json
 from servo_driver import ServoDriver
+from camera import Camera
 
 
 class Auto:
+    def __init__(self):
+        self.camera = Camera()
+        self.servos = ServoDriver()
+
+        json_file = open("model.json", "r")
+        loaded_model_json = json_file.read()
+        json_file.close()
+
+        self.model = model_from_json(loaded_model_json)
+        self.model.load_weights("model.h5")
+
+
     def drive(self):
         print('>> Autonomous driving <<')
 
-        servos = ServoDriver()
-
         while True:
-            servos.set_throttle(config.THROTTLE_MAX) # trololololol
+            frame = self.camera.capture()
+            prediction = self.model.predict(frame)
+
+            print(prediction)
+
+            self.servos.set_throttle(config.THROTTLE_MAX) # trololololol
