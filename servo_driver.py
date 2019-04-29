fd97e11e8bedfcb2672f5413f1251816978867d1
Joe Love <git@joelove.uk>
2019-04-29 20:15:36 +0100

    modify actuation range

--- servo_driver.py
+++ servo_driver.py
@@ -6,8 +6,9 @@ from adafruit_servokit import ServoKit
 class ServoDriver:
     def __init__(self, channels=16):
         kit = ServoKit(channels=channels)
-        self.steering_servo = kit.servo[config.STEERING_CHANNEL]
         self.throttle_servo = kit.servo[config.THROTTLE_CHANNEL]
+        self.steering_servo = kit.servo[config.STEERING_CHANNEL]
+        self.steering_servo.actuation_range = config.STEERING_RANGE
 
     def set_angle(self, angle):
         self.steering_servo.angle = angle
