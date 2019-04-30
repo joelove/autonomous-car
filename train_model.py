471ae2b0f17481b14564e28cbcbd88bfe23e8996
Joe Love <git@joelove.uk>
2019-04-30 20:22:12 +0100

    update cnn shape

--- train_model.py
+++ train_model.py
@@ -8,7 +8,7 @@ import config
 
 from funcy import rcompose as pipe
 from keras.models import Model
-from keras.layers import Input, Cropping3D, Convolution3D, MaxPooling3D, BatchNormalization, Dropout, Flatten, Dense
+from keras.layers import Input, Cropping3D, Convolution2D, MaxPooling3D, BatchNormalization, Dropout, Flatten, Dense
 
 
 def save_model(model):
@@ -19,10 +19,10 @@ def save_model(model):
 
 
 def create_model():
-    layers = pipe(
-        Cropping3D(cropping=((0, 0), (60, 0), (0, 0))),
-        Convolution3D(8, (3, 3, 3), strides=(1, 2, 2), activation='relu'),
-        MaxPooling3D(pool_size=(1, 2, 2)),
+    image_input = Input(shape=(120, 160, 1))
+
+    hidden_layers = pipe(
+        Convolution2D(filters=24, kernel_size=(5, 5), strides=(2, 2), activation='relu'),
         BatchNormalization(),
         Dropout(0.1),
         Flatten(name='flattened'),
@@ -30,17 +30,19 @@ def create_model():
         Dropout(0.2),
     )
 
-    angle_layer = Dense(15, activation='softmax', name='angle_output')
-    throttle_layer = Dense(1, activation='relu', name='throttle_output')
+    angle_output_layer = pipe(
+        Dense(1, activation='sigmoid'),
+        Dense(1, activation='linear', name='angle_output')
+    )
 
-    image_input = Input(shape=(3, 120, 160, 3))
+    throttle_output_layer = Dense(1, activation='sigmoid', name='throttle_output')
 
-    angle_output = pipe(layers, angle_layer)(image_input)
-    throttle_output = pipe(layers, throttle_layer)(image_input)
+    angle_output = pipe(hidden_layers, angle_output_layer)(image_input)
+    throttle_output = pipe(hidden_layers, throttle_output_layer)(image_input)
 
     model = Model(inputs=[image_input], outputs=[angle_output, throttle_output])
     model.compile(optimizer='adam',
-                  loss={'angle_output':'categorical_crossentropy', 'throttle_output': 'mean_absolute_error'},
+                  loss={'angle_output':'mean_absolute_error', 'throttle_output': 'mean_absolute_error'},
                   loss_weights={'angle_output': 0.9, 'throttle_output': 0.01})
 
     return model
@@ -50,8 +52,9 @@ def train_model():
     record_files = glob.glob(f'{config.DATA_PATH}/*.json')
     total_records = len(record_files)
 
-    X_train = []
-    Y_train = []
+    frames = []
+    angles = []
+    throttles = []
 
     for index, filepath in enumerate(record_files):
         completion_ratio = float(index) / total_records
@@ -64,37 +67,42 @@ def train_model():
 
             angle = record["angle"]
             throttle = record["throttle"]
+
+            angles.append(angle)
+            throttles.append(throttle)
+
             frame_filename = record["frame_filename"]
 
-            image_array = cv2.imread(f'{config.DATA_PATH}/{frame_filename}')
-            image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
-            image_array = image_array / 255.0
-            image_array = image_array.reshape(image_array.shape + (1,))
+            frame_array = cv2.imread(f'{config.DATA_PATH}/{frame_filename}')
+            frame_array = cv2.cvtColor(frame_array, cv2.COLOR_BGR2GRAY)
+            frame_array = frame_array.reshape(frame_array.shape + (1,))
+            frame_array = frame_array / 255.0
 
-            X_train.append(image_array)
-            Y_train.append((angle, throttle))
+            frames.append(frame_array)
 
-    print(f'{total_records} records processed!')
-
-    X_train = np.array(X_train)
-    Y_train = np.array(Y_train)
+    print(f'{total_records} records processed!', 99*' ')
 
     print("Creating model...", end="\r")
+
     model = create_model()
-    print("Model created!")
+
+    print("Model created!", 99*' ')
 
     print("Training model...", end="\r")
-    model.fit(X_train, Y_train, validation_split=0.1, epochs=5, verbose=1)
-    print("Model trained!")
+
+    X_train = np.array(frames)
+    Y_train = [np.array(angles), np.array(throttles)]
+
+    model.fit(X_train, Y_train, validation_split=0.2, epochs=5, verbose=1)
+
+    print("Model trained!", 99*' ')
 
     print("Saving model...", end="\r")
+
     save_model(model)
-    print("Model saved!")
+
+    print("Model saved!", 99*' ')
 
 
 if __name__ == "__main__":
     train_model()
-
-# 58022_cam-image_array_.jpg
-# record_58022.json
-# {"cam/image_array": "58022_cam-image_array_.jpg", "user/throttle": -0.24747459334086122, "user/mode": "user", "timestamp": "2019-04-15 16:33:41.246618", "user/angle": 0.0}
