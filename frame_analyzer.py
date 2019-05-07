903eb0fec43675fdbe6bf0badfaa95f88df60abc
Joe Love <git@joelove.uk>
2019-05-07 15:17:09 +0100

    remove old code

--- frame_analyzer.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import image_processor
-
-def handle_frame(frame):
-    filtered_image = image_processor.apply_filters(frame)
-
-    cv2.imwrite('preview.jpg', filtered_image)
-    # cv2.imshow('Preview', filtered_image)
--- test.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-i = 1
-while True:
-    print(f'Processing records ({i}%)', end="\r")
-    i = i + 1
