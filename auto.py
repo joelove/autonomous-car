159de48f6eed6985138405d7133bbf05fe08e02b
Joe Love <git@joelove.uk>
2019-05-07 13:17:39 +0100

    fix throttle

--- auto.py
+++ auto.py
@@ -77,7 +77,7 @@ class Auto:
                 throttle = self.interval_to_throttle(throttle_interval)
 
                 self.servos.set_angle(angle)
-                self.servos.set_throttle(0.275)
+                self.servos.set_throttle(0.285)
 
                 frame = np.array([])
 
