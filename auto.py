fb55b95ee9bb0fdaa9fad30b33a22656be94dd24
Joe Love <git@joelove.uk>
2019-05-07 18:08:10 +0100

    add vstack

--- auto.py
+++ auto.py
@@ -47,6 +47,8 @@ class Auto:
 
 
     def process_frame(self, frame):
+        padded_frame = np.vstack((frame, np.zeros(160, 8)))
+
         frame_array = frame.reshape((1,) + frame.shape + (1,))
         frame_array = frame_array / 255.0
 
