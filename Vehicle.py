6d96a78517b84d24ca7a094dd0f7e627259931f6
Joe Love <git@joelove.uk>
2019-05-08 15:24:02 +0100

    revert exponent change

--- Vehicle.py
+++ Vehicle.py
@@ -15,8 +15,8 @@ class Vehicle:
 
 
     def number_to_exponential(self, axis):
-        cube_axis = axis ** 2
-        exponential_axis = -cube_axis if axis else cube_axis
+        cube_axis = axis ** 3
+        exponential_axis = cube_axis if axis else -cube_axis
 
         return exponential_axis
 
