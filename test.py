edb9a917309f9eafbb6435c7ef009b8c2e2ff6d9
Joe Love <git@joelove.uk>
2019-05-21 16:56:40 +0100

    try with int, hmm

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ servos = ServoDriver()
 try:
     while True:
         raw_value = input("Enter speed: ")
-        servo_value = float(raw_value)
+        servo_value = int(raw_value)
         servos.set_throttle(servo_value)
 except KeyboardInterrupt:
     pass
