f1b0195ec1b3ed2ae42104281f0f95b7be0d9c34
Joe Love <git@joelove.uk>
2019-05-21 17:38:45 +0100

    continuous servo it is

--- ServoDriver.py
+++ ServoDriver.py
@@ -1,5 +1,5 @@
 from adafruit_pca9685 import PCA9685
-from adafruit_motor import servo, motor
+from adafruit_motor import servo
 from board import SCL, SDA
 
 import busio
@@ -13,7 +13,10 @@ class ServoDriver:
         pca = PCA9685(i2c)
         pca.frequency = 50
 
-        self.throttle_servo = motor.DCMotor(pca.channels[config.THROTTLE_CHANNEL])
+        self.throttle_servo = servo.ContinuousServo(pca.channels[config.THROTTLE_CHANNEL],
+            min_pulse=config.THROTTLE_MIN_PULSE,
+            max_pulse=config.THROTTLE_MAX_PULSE)
+
         self.steering_servo = servo.Servo(pca.channels[config.STEERING_CHANNEL],
             min_pulse=config.STEERING_MIN_PULSE,
             max_pulse=config.STEERING_MAX_PULSE)
