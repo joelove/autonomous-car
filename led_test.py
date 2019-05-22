8ed0bc1ecf8d2b305af230d237eaf6ef67a6ea8d
Joe Love <git@joelove.uk>
2019-05-22 12:54:57 +0100

    add dbug lines

--- led_test.py
+++ led_test.py
@@ -7,7 +7,9 @@ GPIO.setwarnings(False)
 GPIO.setup(13, GPIO.OUT)
 
 while True:
+    print('HIGH')
     GPIO.output(13, GPIO.HIGH)
     time.sleep(0.5)
+    print('LOW')
     GPIO.output(13, GPIO.LOW)
     time.sleep(0.5)
