af63e10a0ca7bfd32ae563b0c0c719989700636e
Joe Love <git@joelove.uk>
2019-05-22 16:02:35 +0100

    Add flash

--- led_test.py
+++ led_test.py
@@ -2,6 +2,7 @@ from adafruit_pca9685 import PCA9685
 from board import SCL, SDA
 
 import busio
+import time
 
 
 i2c = busio.I2C(SCL, SDA)
@@ -10,4 +11,11 @@ pca = PCA9685(i2c)
 pca.frequency = 60
 
 led_channel = pca.channels[8]
-led_channel.duty_cycle = 0xffff
+
+while True:
+    print('ON')
+    led_channel.duty_cycle = 0xffff
+    time.sleep(0.5)
+    print('OFF')
+    led_channel.duty_cycle = 0
+    time.sleep(0.5)
