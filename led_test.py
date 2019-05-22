9cf21b4c855c4e1abbde574ffdbd208c9d3e4011
Joe Love <git@joelove.uk>
2019-05-22 16:40:17 +0100

    split leds in test

--- led_test.py
+++ led_test.py
@@ -10,12 +10,18 @@ i2c = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c)
 pca.frequency = 60
 
-led_channel = pca.channels[8]
+RED_LED_CHANNEL = 8
+WHITE_LED_CHANNEL = 9
+
+red_led = pca.channels[RED_LED_CHANNEL]
+white_led = pca.channels[WHITE_LED_CHANNEL]
 
 while True:
-    print('ON')
-    led_channel.duty_cycle = 0xffff
-    time.sleep(0.5)
-    print('OFF')
-    led_channel.duty_cycle = 0
-    time.sleep(0.5)
+    red_led.duty_cycle = 0xffff
+    time.sleep(0.25)
+    white_led.duty_cycle = 0xffff
+    time.sleep(0.25)
+    red_led.duty_cycle = 0
+    time.sleep(0.25)
+    white_led.duty_cycle = 0
+    time.sleep(0.25)
