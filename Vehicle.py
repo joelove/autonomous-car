47d4e806ca066c5e9824fabd3bb98748f2195c12
Joe Love <git@joelove.uk>
2019-05-23 16:38:41 +0100

    can we get rid of the noise?

--- Vehicle.py
+++ Vehicle.py
@@ -34,7 +34,9 @@ class Vehicle:
         throttle_exponential_interval = self.number_to_exponential(throttle_interval)
         throttle_inverse = self.interval_to_inverse(throttle_exponential_interval)
 
-        return throttle_inverse + 0.1
+        print(throttle_inverse)
+
+        return throttle_inverse + 0.2
 
 
     def interval_to_steering_angle(self, interval):
