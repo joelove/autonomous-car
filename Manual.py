40c2bb7ec2bf130ef7fdf468a5cdc2b1219efbff
Joe Love <git@joelove.uk>
2019-06-03 13:23:54 +0100

    little bit quicker

--- Manual.py
+++ Manual.py
@@ -78,6 +78,8 @@ class Manual(Vehicle):
                 angle = self.interval_to_steering_angle(steering_interval)
                 throttle = self.interval_to_throttle(throttle_interval)
 
+                print(throttle)
+
                 self.servos.set_angle(angle)
                 self.servos.set_throttle(throttle)
 
