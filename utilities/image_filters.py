36937ccf12352053a9028e2ea7dd1a5712b5dd0a
Joe Love <git@joelove.uk>
2019-06-03 19:27:58 +0100

    maybe it's proportional? so tired out that i'm guessing about my own math

--- utilities/image_filters.py
+++ utilities/image_filters.py
@@ -23,13 +23,12 @@ def crop_hood(image):
     right_image = image[:, last_third:]
     center_image = image[:, first_third:last_third]
 
-    hood_height = 0.62
+    hood_height = 0.36
+    horizon_height = 0.42
+    peripheral_height = 0.49
 
     original_image_shape = [(0, 0), (1, 0), (1, 1), (0, 1)]
     cropped_image_shape = [(0, 0), (1, 0), (1, hood_height), (0, hood_height)]
-
-    horizon_height = 0.42
-    peripheral_height = 0.49
 
     left_horizon_shape = [(0, peripheral_height), (1, horizon_height), (1, 1), (0, 1)]
     right_horizon_shape = [(0, horizon_height), (1, peripheral_height), (1, 1), (0, 1)]
