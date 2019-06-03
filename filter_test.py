99cf62514b87c05596ecfcbad3d760a352d195e8
Joe Love <git@joelove.uk>
2019-06-03 15:23:40 +0100

    add filters to filter test

--- filter_test.py
+++ filter_test.py
@@ -2,11 +2,14 @@ import time
 import cv2
 
 from utilities.stream_pipelines import gstreamer_pipeline
+from utilities.image_filters import apply_default_filters
 
 capture = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
 
 while capture.isOpened():
     success, frame = capture.read()
+
+    frame = apply_default_filters(frame)
 
     if success:
         cv2.imshow(frame)
