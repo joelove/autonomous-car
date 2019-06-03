69c118a4466a2fd1ab46c70ad94bdefdc56795ad
Joe Love <git@joelove.uk>
2019-06-03 10:41:34 +0100

    remove now uneeded debug

--- Camera.py
+++ Camera.py
@@ -10,8 +10,9 @@ from utilities.stream_pipelines import gstreamer_pipeline
 class Camera:
     def __init__(self):
         print(gstreamer_pipeline())
-        print(cv2.CAP_GSTREAMER)
+
         self.capture = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+
         self.frames = Queue()
         self.thread = Thread(target=self.capture_continuous, daemon=True, args=(self.frames,))
         self.thread.start()
@@ -20,16 +21,13 @@ class Camera:
     def capture_continuous(self, frames):
         tick_length = 1.0 / config.CAMERA_FRAMERATE
 
-        while True:
+        while self.capture.isOpened():
             start_time = time.time()
 
             if not frames.full():
                 success, frame = self.capture.read();
-                print(frame)
 
                 if success:
-                    print('success')
                     frames.put_nowait(frame)
-                    print('PUT')
 
             time.sleep(tick_length - ((time.time() - start_time) % tick_length))
